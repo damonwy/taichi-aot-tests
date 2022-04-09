344702cf10b40861bd4239e27df09859a689c83f
Ailing Zhang <ailing@taichi.graphics>
2022-04-09 14:29:01 +0800

    Add python script for implicit fem

--- python/implicit_fem.py
+++ python/implicit_fem.py
@@ -1,4 +1,3 @@
-from model_data import *
 import argparse, re, pathlib, shutil, os
 
 import numpy as np
@@ -7,155 +6,30 @@ from taichi._lib import core as _ti_core
 import taichi as ti
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--exp',
-                    choices=['implicit', 'explicit'],
-                    default='implicit')
 parser.add_argument('--dim', type=int, default=3)
-parser.add_argument('--gui', choices=['auto', 'ggui', 'cpu'], default='auto')
-#parser.add_argument('--mesh', default=None)
-#parser.add_argument('--mesh', default='/media/hdd/model/scale/bunny/bunny0.1.node')
-#parser.add_argument('--mesh', default='./armadillo0.1.node')
-parser.add_argument('--mesh', default='./fox.1.node')
 parser.add_argument('--aot', default=False, action='store_true')
-parser.add_argument('place_holder', nargs='*')
 args = parser.parse_args()
 
-ti.init(arch=ti.vulkan, dynamic_index=False)
+# FIXME: ndarray annotation
 
-if args.gui == 'auto':
-    if _ti_core.GGUI_AVAILABLE:
-        args.gui = 'ggui'
-    else:
-        args.gui = 'cpu'
+# TODO: asserts cuda or vulkan backend
+ti.init(arch=ti.vulkan)
 
-if 'ele_str' in locals():
-    def read_np(str, dim, face_flag=False, node_flag=False):
-        data = str.split('\n')
-        n = int(re.findall(r'\S+', data[0])[0])
-        array_np = []
-        for line in data[1: n + 1]:
-            xs = re.findall(r'\S+', line)[1: dim + 1]
-            #if face_flag: xs[0], xs[1] = xs[1], xs[0] # flip the face
-            if node_flag: array_np.append([float(i) for i in xs])
-            else : array_np.append([int(i) for i in xs])
-        return [n, np.array(array_np)]
+c2e_np = np.load('c2e.npy')
+vertices_np = np.load('vertices_np.npy')
+indices_np = np.load('indices_np.npy')
+edges_np = np.load('edges_np.npy')
+ox_np = np.load('ox_np.npy')
 
-    n_verts, array_np = read_np(node_str, 3, node_flag=True)
-    ma = 0
-    for i in range(3):
-        array_np[:, i] -= array_np[:, i].min()
-        ma = max(ma, array_np[:, i].max())
-    array_np /= ma
-    array_np[:, 2] *= -1
-    ox_np = array_np
-
-    n_cells, vertices_np = read_np(ele_str, 4)
-
-    n_faces, indices_np = read_np(face_str, 3, face_flag=True)
-
-    edges_np = set()
-    for i in vertices_np:
-        for u in i:
-            for v in i:
-                if u < v:
-                    edges_np.add((u, v))
-    edges_np = np.array(list(sorted(edges_np)))
-    n_edges = len(edges_np)
-
-    map = {}
-    for i, j in enumerate(edges_np):
-        map[tuple(j)] = i
-    c2e_np = []
-    for i in vertices_np:
-        tmp = []
-        for u in i:
-            for u1 in i:
-                if u < u1:
-                    tmp.append(map[(u, u1)])
-        c2e_np.append(tmp)
-    c2e_np = np.array(c2e_np)
-
-    def serialize():
-        def write_array(x, t, name, f):
-            if t is float:
-                f.write('float {}'.format(name))
-            else:
-                f.write('int {}'.format(name))
-            if len(x.shape) == 2:
-                f.write('[{}][{}] = '.format(x.shape[0], x.shape[1]))
-                f.write('{')
-                for i in range(x.shape[0]):
-                    f.write('{')
-                    for j in range(x.shape[1]):
-                        f.write(str(t(x[i, j])) + ',')
-                    f.write('},')
-                f.write('};\n')
-            elif len(x.shape) == 1:
-                f.write('[{}] = '.format(x.shape[0]))
-                f.write('{')
-                for i in range(x.shape[0]):
-                    f.write(str(t(x[i])) + ',')
-                f.write('};\n')
-        data_header_path = '../include/data.h'
-        os.remove(data_header_path)
-        with open(data_header_path, 'w') as f:
-            f.write('constexpr int N_VERTS = {};\n'.format(n_verts))
-            f.write('constexpr int N_CELLS = {};\n'.format(n_cells))
-            f.write('constexpr int N_FACES = {};\n'.format(n_faces))
-            f.write('constexpr int N_EDGES = {};\n'.format(n_edges))
-            write_array(ox_np, float, 'ox_data', f)
-            write_array(vertices_np, int, 'vertices_data', f)
-            write_array(indices_np.reshape(-1), int, 'indices_data', f)
-            write_array(edges_np, int, 'edges_data', f)
-            write_array(c2e_np, int, 'c2e_data', f)
-    serialize()
-# else: # read mesh from tetgen output file
-#     res = re.search(r'^(.+)\.(\w+)$', args.mesh)
-#     if res is not None and res.group(2) in ['node', 'ele', 'face']:
-#         args.mesh = res.group(1)
-#     def read_np(filename, dim):
-#         with open(filename, 'r') as fi:
-#             data = fi.readlines()
-#             n = int(re.findall(r'\S+', data[0])[0])
-#             array_np = []
-#             for line in data[1: n + 1]:
-#                 xs = re.findall(r'\S+', line)[1: dim + 1]
-#                 if filename[-4:] == 'face': xs[0], xs[1] = xs[1], xs[0] # flip the face
-#                 if filename[-4:] == 'node': array_np.append([float(i) for i in xs])
-#                 else : array_np.append([int(i) for i in xs])
-#             return [n, np.array(array_np)]
-
-#     n_verts, array_np = read_np(f'{args.mesh}.node', 3)
-#     ma = 0
-#     for i in range(3):
-#         array_np[:, i] -= array_np[:, i].min()
-#         ma = max(ma, array_np[:, i].max())
-#     array_np /= ma
-#     ox = ti.Vector.field(args.dim, dtype=ti.f32, shape=n_verts)
-#     ox.from_numpy(array_np)
-
-#     n_cells, array_np = read_np(f'{args.mesh}.ele', 4)
-#     vertices = ti.Vector.field(4, dtype=ti.i32, shape=n_cells)
-#     vertices.from_numpy(array_np)
-
-#     n_faces, array_np = read_np(f'{args.mesh}.face', 3)
-#     indices = ti.field(ti.i32, shape=n_faces * 3)
-#     indices.from_numpy(array_np.reshape(-1))
-
+n_edges = edges_np.shape[0]
+n_verts = ox_np.shape[0]
+n_cells = c2e_np.shape[0]
+n_faces = indices_np.shape[0]
 
 E, nu = 5e4, 0.0
 mu, la = E / (2 * (1 + nu)), E * nu / ((1 + nu) * (1 - 2 * nu))  # lambda = 0
 density = 1000.0
-dt_explicit = 2e-4
-#dt_implicit = 1e-2
-dt_implicit = 2.5e-3
-#num_substep = int(dt_implicit / dt_explicit + 0.5)
-
-if args.exp == 'implicit':
-    dt = dt_implicit
-
-if args.exp == 'explicit':
-    dt = dt_explicit
+dt = 2.5e-3
 
 num_substep = int(1e-2 / dt + 0.5)
 
@@ -168,7 +42,7 @@ m = ti.field(dtype=ti.f32, shape=n_verts)
 B = ti.Matrix.field(args.dim, args.dim, dtype=ti.f32, shape=n_cells)
 W = ti.field(dtype=ti.f32, shape=n_cells)
 
-gravity = ti.Vector.field(3, dtype=ti.f32, shape=())
+gravity = [0, -9.8, 0]
 
 ox = ti.Vector.ndarray(args.dim, dtype=ti.f32, shape=n_verts)
 vertices = ti.Vector.ndarray(4, dtype=ti.i32, shape=n_cells)
@@ -203,9 +77,10 @@ def clear_field():
     for I in ti.grouped(hes_vert):
         hes_vert[I] = 0
 
+
 @ti.func
 def Ds(verts, x: ti.template()):
-    return ti.Matrix.cols([x[verts[i]] - x[verts[3]] for i in range(3)])
+    return ti.Matrix.cols([x[verts[i]] - x[verts[3]] + 1e-5 for i in range(3)])
 
 
 @ti.func
@@ -236,7 +111,8 @@ def get_force_func(c, verts, x: ti.template(), f: ti.template()):
 
 
 @ti.kernel
-def get_force(x: ti.any_arr(), f: ti.any_arr(), vertices: ti.any_arr(), g_x: ti.f32, g_y: ti.f32, g_z: ti.f32):
+def get_force(x: ti.any_arr(), f: ti.any_arr(), vertices: ti.any_arr(),
+              g_x: ti.f32, g_y: ti.f32, g_z: ti.f32):
     for c in vertices:
         get_force_func(c, vertices[c], x, f)
     for u in f:
@@ -278,29 +154,6 @@ def get_matrix(c2e: ti.any_arr(), vertices: ti.any_arr()):
         for zz in ti.static(range(4)):
             hes_vert[verts[zz]] += hes[zz * 3, zz * 3]
 
-# @ti.kernel
-# def matmul_cell(ret: ti.template(), vel: ti.template()):
-#     for i in ret:
-#         ret[i] = vel[i] * m[i]
-#     for c in vertices:
-#         verts = vertices[c]
-#         W_c = W[c]
-#         B_c = B[c]
-#         for u in range(4):
-#             for d in range(3):
-#                 dD = ti.Matrix.zero(ti.f32, 3, 3)
-#                 if u == 3:
-#                     for j in range(3):
-#                         dD[d, j] = -1
-#                 else:
-#                     dD[d, u] = 1
-#                 dF = dD @ B_c
-#                 dP = 2.0 * mu * dF
-#                 dH = -W_c * dP @ B_c.transpose()
-#                 for i in range(3):
-#                     for j in range(3):
-#                         tmp = (vel[verts[i]][j] - vel[verts[3]][j])
-#                         ret[verts[u]][d] += -dt**2 * dH[j, i] * tmp
 
 @ti.kernel
 def matmul_edge(ret: ti.any_arr(), vel: ti.any_arr(), edges: ti.any_arr()):
@@ -318,8 +171,10 @@ def add(ans: ti.any_arr(), a: ti.any_arr(), k: ti.f32, b: ti.any_arr()):
     for i in ans:
         ans[i] = a[i] + k * b[i]
 
+
 @ti.kernel
-def add_hack(ans: ti.any_arr(), a: ti.any_arr(), k: ti.f32, scalar: ti.any_arr(), b: ti.any_arr()):
+def add_scalar_ndarray(ans: ti.any_arr(), a: ti.any_arr(), k: ti.f32,
+             scalar: ti.any_arr(), b: ti.any_arr()):
     for i in ans:
         ans[i] = a[i] + k * scalar[None] * b[i]
 
@@ -348,13 +203,16 @@ def fill_ndarray(ndarray: ti.any_arr(), val: ti.f32):
     for I in ti.grouped(ndarray):
         ndarray[I] = [val, val, val]
 
+
 @ti.kernel
 def init_r_2():
     r_2_scalar[None] = dot_ans[None]
 
+
 @ti.kernel
 def update_alpha(alpha_scalar: ti.any_arr()):
     alpha_scalar[None] = r_2_scalar[None] / dot_ans[None]
+
 
 @ti.kernel
 def update_beta_r_2(beta_scalar: ti.any_arr()):
@@ -363,7 +221,7 @@ def update_beta_r_2(beta_scalar: ti.any_arr()):
 
 
 def cg(it):
-    get_force(x, f, vertices, 3, -9.8, 0)
+    get_force(x, f, vertices, gravity[0], gravity[1], gravity[2])
     get_b(v, b, f)
     matmul_edge(mul_ans, v, edges)
     matmul_edge(mul_ans, v, edges)
@@ -377,11 +235,11 @@ def cg(it):
         matmul_edge(mul_ans, p0, edges)
         dot2scalar(p0, mul_ans)
         update_alpha(alpha_scalar)
-        add_hack(v, v, 1, alpha_scalar, p0)
-        add_hack(r0, r0, -1, alpha_scalar, mul_ans)
+        add_scalar_ndarray(v, v, 1, alpha_scalar, p0)
+        add_scalar_ndarray(r0, r0, -1, alpha_scalar, mul_ans)
         dot2scalar(r0, r0)
         update_beta_r_2(beta_scalar)
-        add_hack(p0, r0, 1, beta_scalar, p0)
+        add_scalar_ndarray(p0, r0, 1, beta_scalar, p0)
     fill_ndarray(f, 0)
     add(x, x, dt, v)
 
@@ -395,8 +253,8 @@ def advect():
 
 
 @ti.kernel
-def init(x: ti.any_arr(), v: ti.any_arr(), f: ti.any_arr(), ox: ti.any_arr(), vertices: ti.any_arr()):
-    gravity[None] = [0, -9.8, 0]
+def init(x: ti.any_arr(), v: ti.any_arr(), f: ti.any_arr(), ox: ti.any_arr(),
+         vertices: ti.any_arr()):
     for u in x:
         x[u] = ox[u]
         v[u] = [0.0] * 3
@@ -408,6 +266,7 @@ def init(x: ti.any_arr(), v: ti.any_arr(), f: ti.any_arr(), ox: ti.any_arr(), ve
         W[c] = ti.abs(F.determinant()) / 6
         for i in ti.static(range(4)):
             m[vertices[c][i]] += W[c] / 4 * density
+
 
 @ti.kernel
 def floor_bound(x: ti.any_arr(), v: ti.any_arr()):
@@ -423,16 +282,9 @@ def floor_bound(x: ti.any_arr(), v: ti.any_arr()):
                     v[u][i] = 0
 
 
-
-
 def substep():
-    if args.exp == 'explicit':
-        for i in range(num_substep):
-            get_force(x, f)
-            advect()
-    else:
-        for i in range(num_substep):
-            cg(i)
+    for i in range(num_substep):
+        cg(i)
     floor_bound(x, v)
 
 
@@ -448,7 +300,7 @@ def run_aot():
     mod.add_kernel(get_matrix, (c2e, vertices))
     mod.add_kernel(matmul_edge, (mul_ans, x, edges))
     mod.add_kernel(add, (x, x, v))
-    mod.add_kernel(add_hack, (x, x, alpha_scalar, v))
+    mod.add_kernel(add_scalar_ndarray, (x, x, alpha_scalar, v))
     mod.add_kernel(dot2scalar, (r0, r0))
     mod.add_kernel(get_b, (v, b, f))
     mod.add_kernel(ndarray_to_ndarray, (p0, r0))
@@ -467,98 +319,110 @@ def convert_to_field(x: ti.any_arr(), y: ti.template()):
         y[I] = x[I]
 
 
+def run_ggui():
+    res = (800, 600)
+    window = ti.ui.Window("Implicit FEM", res, vsync=True)
+
+    frame_id = 0
+    canvas = window.get_canvas()
+    scene = ti.ui.Scene()
+    camera = ti.ui.make_camera()
+    camera.position(0.0, 1.5, 2.95)
+    camera.lookat(0.0, 0.0, 0.0)
+    camera.fov(55)
+
+    x_field = ti.Vector.field(3, dtype=ti.f32, shape=n_verts)
+
+    def handle_interaction(window):
+        global gravity
+        gravity = [0, -9.8, 0]
+        if window.is_pressed('i'):
+            gravity = [0, 0, -9.8]
+        if window.is_pressed('k'):
+            gravity = [0, 0, 9.8]
+        if window.is_pressed('o'):
+            gravity = [0, 9.8, 0]
+        if window.is_pressed('u'):
+            gravity = [0, -9.8, 0]
+        if window.is_pressed('l'):
+            gravity = [9.8, 0, 0]
+        if window.is_pressed('j'):
+            gravity = [-9.8, 0, 0]
+
+    def render():
+        convert_to_field(x, x_field)
+        camera.track_user_inputs(window,
+                                 movement_speed=0.03,
+                                 hold_key=ti.ui.RMB)
+        scene.set_camera(camera)
+
+        scene.ambient_light((0.1, ) * 3)
+
+        scene.point_light(pos=(0.5, 10.0, 0.5), color=(0.5, 0.5, 0.5))
+        scene.point_light(pos=(10.0, 10.0, 10.0), color=(0.5, 0.5, 0.5))
+
+        scene.mesh(x_field, indices, color=(0.73, 0.33, 0.23))
+
+        canvas.scene(scene)
+
+    while window.running:
+        frame_id += 1
+        frame_id = frame_id % 256
+        substep()
+        if window.is_pressed('r'):
+            init(x, v, f)
+        if window.is_pressed(ti.GUI.ESCAPE):
+            break
+        handle_interaction(window)
+
+        render()
+
+        window.show()
+
+
+def generate_data_header_file_for_aot():
+    def write_array(x, t, name, f):
+        if t is float:
+            f.write('float {}'.format(name))
+        else:
+            f.write('int {}'.format(name))
+        if len(x.shape) == 2:
+            f.write('[{}][{}] = '.format(x.shape[0], x.shape[1]))
+            f.write('{')
+            for i in range(x.shape[0]):
+                f.write('{')
+                for j in range(x.shape[1]):
+                    f.write(str(t(x[i, j])) + ',')
+                f.write('},')
+            f.write('};\n')
+        elif len(x.shape) == 1:
+            f.write('[{}] = '.format(x.shape[0]))
+            f.write('{')
+            for i in range(x.shape[0]):
+                f.write(str(t(x[i])) + ',')
+            f.write('};\n')
+
+    data_header_path = '../include/data.h'
+    if os.path.exists(data_header_path):
+        os.remove(data_header_path)
+    with open(data_header_path, 'w') as f:
+        f.write('constexpr int N_VERTS = {};\n'.format(n_verts))
+        f.write('constexpr int N_CELLS = {};\n'.format(n_cells))
+        f.write('constexpr int N_FACES = {};\n'.format(n_faces))
+        f.write('constexpr int N_EDGES = {};\n'.format(n_edges))
+        write_array(ox_np, float, 'ox_data', f)
+        write_array(vertices_np, int, 'vertices_data', f)
+        write_array(indices_np.reshape(-1), int, 'indices_data', f)
+        write_array(edges_np, int, 'edges_data', f)
+        write_array(c2e_np, int, 'c2e_data', f)
+
+
 if __name__ == '__main__':
     if args.aot:
         run_aot()
+        generate_data_header_file_for_aot()
     else:
         clear_field()
         init(x, v, f, ox, vertices)
         get_matrix(c2e, vertices)
-
-        def handle_interaction(window):
-            #print(window.event)
-            gravity[None] = [0, -9.8, 0]
-            if window.is_pressed('i'):
-                gravity[None] = [0, 0, -9.8]
-            if window.is_pressed('k'):
-                gravity[None] = [0, 0, 9.8]
-            if window.is_pressed('o'):
-                gravity[None] = [0, 9.8, 0]
-            if window.is_pressed('u'):
-                gravity[None] = [0, -9.8, 0]
-            if window.is_pressed('l'):
-                gravity[None] = [9.8, 0, 0]
-            if window.is_pressed('j'):
-                gravity[None] = [-9.8, 0, 0]
-
-
-        if args.gui == 'ggui':
-            res = (800, 600)
-            window = ti.ui.Window("Implicit FEM", res, vsync=True)
-
-            frame_id = 0
-            canvas = window.get_canvas()
-            scene = ti.ui.Scene()
-            camera = ti.ui.make_camera()
-            camera.position(0.0, 1.5, 2.95)
-            camera.lookat(0.0, 0.0, 0.0)
-            camera.fov(55)
-
-            x_field = ti.Vector.field(3, dtype=ti.f32, shape=n_verts)
-
-            def render():
-                convert_to_field(x, x_field)
-                camera.track_user_inputs(window,
-                                         movement_speed=0.03,
-                                         hold_key=ti.ui.RMB)
-                scene.set_camera(camera)
-
-                scene.ambient_light((0.1, ) * 3)
-
-                scene.point_light(pos=(0.5, 10.0, 0.5), color=(0.5, 0.5, 0.5))
-                scene.point_light(pos=(10.0, 10.0, 10.0), color=(0.5, 0.5, 0.5))
-
-                scene.mesh(x_field, indices, color=(0.73, 0.33, 0.23))
-
-                canvas.scene(scene)
-
-            while window.running:
-                frame_id += 1
-                frame_id = frame_id % 256
-                substep()
-                print(x[0][0], x[0][1], x[0][2])
-                if window.is_pressed('r'):
-                    init(x, v, f)
-                if window.is_pressed(ti.GUI.ESCAPE):
-                    break
-                handle_interaction(window)
-
-                render()
-
-                window.show()
-
-        else:
-
-            def T(a):
-
-                phi, theta = np.radians(28), np.radians(32)
-
-                a = a - 0.2
-                x, y, z = a[:, 0], a[:, 1], a[:, 2]
-                c, s = np.cos(phi), np.sin(phi)
-                C, S = np.cos(theta), np.sin(theta)
-                x, z = x * c + z * s, z * c - x * s
-                u, v = x, y * C + z * S
-                return np.array([u, v]).swapaxes(0, 1) + 0.5
-
-            gui = ti.GUI('Implicit FEM')
-            while gui.running:
-                substep()
-                if gui.get_event(ti.GUI.PRESS):
-                    if gui.event.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]: break
-                if gui.is_pressed('r'):
-                    init(x, v, f)
-                handle_interaction(gui)
-                gui.clear(0x000000)
-                gui.circles(T(x.to_numpy() / 3), radius=1.5, color=(1.0, 1.0, 1.0))
-                gui.show()
+        run_ggui()
